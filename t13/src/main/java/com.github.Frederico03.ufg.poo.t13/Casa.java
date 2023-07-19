ac297bc7d483541b657018de3166935329ca7375
Frederico03 <fredericogarcez13@gmail.com>
2023-07-19 20:16:35 -0300

    ADD T13-T18

---
+++ t13/src/main/java/com.github.Frederico03.ufg.poo.t13/Casa.java
@@ -0,0 +1,21 @@
+package com.github.Frederico03.ufg.poo.t13;
+
+public class Casa {
+    private String endereco;
+    private int comodos;
+
+    public Casa(String endereco, int comodos) {
+        this.endereco = endereco;
+        this.comodos = comodos;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Número de cômodos: " + comodos);
+    }
+
+    public static void main(String[] args) {
+        Casa minhaCasa
+    }
+
+}
---
+++ t13/src/main/java/com.github.Frederico03.ufg.poo.t13/Pessoa.java
@@ -0,0 +1,11 @@
+package com.github.Frederico03.ufg.poo.t13;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
